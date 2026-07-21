ae8d7df988a6c1f7fd9c584518cdf165971f5494
SakshamRijal1 <sakshamrijal07@gmail.com>
2026-07-21 23:58:32 +0545

    Added optimal approach for pairsum using two pointer

--- DataStructure/Array/Pairsum.cpp
+++ DataStructure/Array/Pairsum.cpp
@@ -1,30 +1,71 @@
 //brute force method
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// vector <int> pairSum(vector <int> &v,int target)
+// {
+//  for (int i=0;i<v.size();i++)
+//  {
+//   for(int j=0;j<v.size();j++)
+//   {
+//     if((v[i]+v[j])==target)
+//     {
+//       return {i,j};
+//     }
+//   }
+//  }
+// }
+// int main()
+// {
+//   vector <int> arr={1,3,2,6};
+//  int target=9;
+//  vector <int> v=pairSum(arr,target);
+//  for(int val :v)
+//  {
+//   cout<<val<<endl;
+//  }
+
+
+
+// }
+
+//optimal aaproach
+
 #include<iostream>
 #include<vector>
 using namespace std;
-vector <int> pairSum(vector <int> &v,int target)
+vector <int> pairSum(vector <int> &v, int target)
 {
- for (int i=0;i<v.size();i++)
- {
-  for(int j=0;j<v.size();j++)
+  vector <int> index;
+   int pairSum=0;
+   int start=0;
+   int end=v.size()-1;
+   for(int i=0;i<v.size();i++)
+   { 
+   pairSum+=(v[start]+v[end]);
+  if(pairSum>target)
   {
-    if((v[i]+v[j])==target)
-    {
-      return {i,j};
-    }
+end--;
+pairSum=0;
   }
- }
+  else if(pairSum<target)
+  {
+start++;
+pairSum=0;
+  }
+  else{
+return {start,end};
+  }
+   }
 }
 int main()
 {
-  vector <int> arr={1,3,2,6};
- int target=9;
- vector <int> v=pairSum(arr,target);
- for(int val :v)
- {
-  cout<<val<<endl;
- }
-
-
+  vector <int> vec={2,7,8,9};
+  int target=10;
+  vector <int> v=pairSum(vec,target);
+  for(int val :v)
+  {
+    cout<<val<<endl;
+  }
 
 }
